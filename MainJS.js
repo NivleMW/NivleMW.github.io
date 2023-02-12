e70114bdd725397c506ab1d1b5fb147b23e41ad9
NivleMW <47514829+NivleMW@users.noreply.github.com>
2023-02-12 22:52:06 +0800

    Updated Timeline (min details)

--- MainJS.js
+++ MainJS.js
@@ -1,4 +1,5 @@
 let allSlidesContainer = document.getElementsByClassName("slidesContainer");
+let currentHistory = null;
 
 function navBarClick(x)
 {
@@ -8,6 +9,28 @@ function navBarClick(x)
 function hover(x)
 {
 	x.classList.toggle("hover");
+}
+
+function selectHistory(x)
+{
+	if (currentHistory == null)
+	{
+		currentHistory = document.querySelector('[data-year="2022"]');
+    }
+	if (currentHistory != x) {
+		let elements;
+		{
+			elements = document.querySelectorAll('[data-year="' + currentHistory.getAttribute("data-year") + '"]');
+			for (i = 0; i < elements.length; i++) {
+				elements[i].classList.toggle("historySelected");
+			}
+		}
+		elements = document.querySelectorAll(`[data-year="${x.getAttribute("data-year")}"]`);
+		for (i = 0; i < elements.length; i++) {
+			elements[i].classList.toggle("historySelected");
+		}
+		currentHistory = x;
+	}
 }
 
 function ShowSlides(slideContainerNo, slideDiff)
